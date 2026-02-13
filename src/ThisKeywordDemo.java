6609b1789952c105edfc433b0a1d3c1c9454d938
Athulkk13579 <athulkrishnak8086@gmail.com>
2026-02-13 22:53:14 +0530

    Implement setValue and display methods in ThisDemo

--- src/ThisKeywordDemo.java
+++ src/ThisKeywordDemo.java
@@ -3,19 +3,24 @@ import java.util.Scanner;
 class ThisDemo {
     int value;
 
-    // TODO: Define setValue(int value)
-    // Use 'this.value = value' to resolve naming conflict
-    
-    // TODO: Define display() to print "Value: " + value
+    void setValue(int value) {
+        this.value = value;
+    }
+
+    void display() {
+        System.out.println("Value: " + value);
+    }
 }
 
-public class ThisKeywordDemo {
+public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // TODO: Read the integer input
+        Scanner scanner = new Scanner(System.in);
+        ThisDemo demo = new ThisDemo();
+
+        int val = scanner.nextInt();
+        demo.setValue(val);
+        demo.display();
         
-        // TODO: Create ThisDemo object
-        // TODO: Call setValue() with the input
-        // TODO: Call display()
+        scanner.close();
     }
 }
